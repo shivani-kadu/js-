a31dcfa6bb6a9a6235d6c5bb5e3a11f9cb365674
Shivani kadu <141450088+shivani-kadu@users.noreply.github.com>
2026-04-16 17:34:19 +0000

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+//Topic conversion 
+
+let score =33;
+console.log(typeof score);//output  number 
+console.log(typeof {score}); //output object
+console.log(typeof (score));//output of this number
+
+//"33" => 33
+//"33abc"=>nan not a number 
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);//output string 
+
