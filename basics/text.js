59cea1f4e3ec1072a064618ab22ec8f3cb35e010
Shivani kadu <141450088+shivani-kadu@users.noreply.github.com>
2026-04-16 17:19:02 +0000

    Datatypes

---
+++ basics/text.js
@@ -0,0 +1,15 @@
+const accountId=  133344;
+let accounEmail="shivani@gmail.com"
+var accountPassword ="jaipur"
+
+
+// prepfer not use var beacuse of issue  
+// in block scope and functional scope 
+// "use strict"; // tresat all js code as newver version 
+
+//null
+//bigint
+//string 
+//undefined type is object 
+//symbol
+//null-object
